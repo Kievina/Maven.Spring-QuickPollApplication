d4648c8c1f93d34b12083393a087a82cc6bbdf20
Kievina <56592501+Kievina@users.noreply.github.com>
2019-12-10 18:57:59 -0500

    add more sql commands

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,6 +9,21 @@ insert into option (option_id, option_value, poll_id) values (5, 'Green fish', 2
 insert into option (option_id, option_value, poll_id) values (6, 'Blue fish', 2);
 
 insert into poll (poll_id, question) values (3, 'What is your favorite dish?');
-insert into option (option_id, option_value, poll_id) values (7, 'Red dish', 3);
-insert into option (option_id, option_value, poll_id) values (8, 'Green dish', 3);
-insert into option (option_id, option_value, poll_id) values (9, 'Blue dish', 3);
+insert into option (option_id, option_value, poll_id) values (7, 'Chicken Teriyaki', 3);
+insert into option (option_id, option_value, poll_id) values (8, 'General Tso', 3);
+insert into option (option_id, option_value, poll_id) values (9, 'Curry Chicken', 3);
+
+insert into poll (poll_id, question) values (4, 'What is your favorite wish?');
+insert into option (option_id, option_value, poll_id) values (10, 'Red wish', 4);
+insert into option (option_id, option_value, poll_id) values (11, 'Green wish', 4);
+insert into option (option_id, option_value, poll_id) values (12, 'Blue wish', 4);
+
+insert into poll (poll_id, question) values (5, 'What is your favorite cat?');
+insert into option (option_id, option_value, poll_id) values (13, 'Red cat', 5);
+insert into option (option_id, option_value, poll_id) values (14, 'Green cat', 5);
+insert into option (option_id, option_value, poll_id) values (15, 'Blue cat', 5);
+
+insert into poll (poll_id, question) values (6, 'What is your favorite hat?');
+insert into option (option_id, option_value, poll_id) values (16, 'Red hat', 6);
+insert into option (option_id, option_value, poll_id) values (17, 'Green hat', 6);
+insert into option (option_id, option_value, poll_id) values (18, 'Blue hat', 6);
